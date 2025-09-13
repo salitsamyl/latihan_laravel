26501240e97158e99c889b9e6cb4d883b5bf169a
salitsamyl <salitsameylani@gmail.com>
2025-09-13 10:35:10 +0700

    createMahsiswa

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use App\Models\Mahasiswa;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/h', function () {
-    return ('Hello World');
-});
+Route::get('/mahasiswa', [MahasiswaController::class,'index']);
+Route::post('/mahasiswa', [MahasiswaController::class,'store']);
